c3f6160d220e5943b504b638b1519a6f85945a47
Gauri Guglani <63574359+gauriguglani11@users.noreply.github.com>
2022-05-01 13:37:06 +0530

    #Day1

---
+++ May 1- Backspacing string.py
@@ -0,0 +1,36 @@
+Question: Given two strings s and t, return true if they are equal when both are typed into empty text editors. 
+'#' means a backspace character.
+Note that after backspacing an empty text, the text will continue empty.
+
+Example 1:
+
+Input: s = "ab#c", t = "ad#c"
+Output: true
+Explanation: Both s and t become "ac".
+
+Example 2:
+
+Input: s = "ab##", t = "c#d#"
+Output: true
+Explanation: Both s and t become "".
+
+Example 3:
+
+Input: s = "a#c", t = "b"
+Output: false
+Explanation: s becomes "c" while t becomes "b".
+
+
+SOLUTION
+
+class Solution(object):
+    def backspaceCompare(self, S, T):
+        def build(S):
+            ans = []
+            for c in S:
+                if c != '#':
+                    ans.append(c)
+                elif ans:
+                    ans.pop()
+            return "".join(ans)
+        return build(S) == build(T)
