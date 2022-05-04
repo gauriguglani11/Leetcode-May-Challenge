a2cacadc1e1d3ca6552d0d0eb75033abd8e0127f
Gauri Guglani <63574359+gauriguglani11@users.noreply.github.com>
2022-05-04 23:56:44 +0530

    #Day4

---
+++ May 4th- Max Number of K-Sum Pairs.py
@@ -0,0 +1,39 @@
+You are given an integer array nums and an integer k.
+
+In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+
+Return the maximum number of operations you can perform on the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4], k = 5
+Output: 2
+Explanation: Starting with nums = [1,2,3,4]:
+- Remove numbers 1 and 4, then nums = [2,3]
+- Remove numbers 2 and 3, then nums = []
+There are no more pairs that sum up to 5, hence a total of 2 operations.
+
+
+SOLUTION:
+
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        left = 0
+        right = len(nums) - 1
+        ans = 0
+        while left < right:
+            cur = nums[left] + nums[right]
+            if cur == k:
+                ans += 1
+                left += 1
+                right -= 1
+            elif cur < k:
+                left += 1
+            else:
+                right -= 1
+        
+        return ans
